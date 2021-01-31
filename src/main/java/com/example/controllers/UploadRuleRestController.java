7ea5aac7a76070506b5bd94c2ce560003b796ee8
ar00062451 <ariel.vaca@prosegur.com>
2021-01-31 17:37:46 -0300

    .

--- src/main/java/com/example/controllers/UploadRuleRestController.java
+++ src/main/java/com/example/controllers/UploadRuleRestController.java
@@ -1,70 +1,26 @@
 package com.example.controllers;
 
 import com.example.config.DroolsBeanFactory;
-import com.example.ruleEngine.drools.resource.service.DroolsRuleService;
 import com.example.utils.DrlManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 @RestController
 @RequestMapping("upload")
 public class UploadRuleRestController {
-    private static final String UPLOAD_PATH = "/src/main/resources/static/upload/";
 
-    //@Autowired
-   // private DroolsRuleService droolsRuleService;
+	@PostMapping("/")
+	public void addRule(@RequestParam("file") MultipartFile file) throws IOException {
 
-    @PostMapping("/")
-    public ResponseEntity addRule(@RequestParam("file") MultipartFile file) {
-       /* try {
-            droolsRuleService.add(file.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+		String drl = new DroolsBeanFactory().getDrlFromExcel(file.getBytes());
+		DrlManager.insertDrl("rule1", drl);
 
-        guardarArchivo(file);
+	}
 
-        return ResponseEntity.ok(null);
-
-    }
-
-    private static String guardarArchivo(MultipartFile file) {
-        String res;
-        if (file.isEmpty()) {
-            res = "Archivo no encontrado";
-        } else {
-            try {
-           //     byte[] bytes = file.getBytes();
-              //  Path path = Paths.get(new File(".").getCanonicalPath() + UPLOAD_PATH + file.getOriginalFilename());
-            //    Files.write(path, bytes);
-             //   String drl = getDrl("static/upload/" + file.getOriginalFilename());
-                String drl =  new DroolsBeanFactory().getDrlFromExcel(file.getBytes());
-                DrlManager.insertDrl("rule1", drl);
-                res = "Archivo subido correctamente" + "\n " + drl;
-
-            } catch (IOException e) {
-                res = "Error al subir archivo: " + e.getMessage();
-            }
-
-        }
-        return res;
-    }
-
-    private static String getDrl(String filePath) {
-        DroolsBeanFactory dbf = new DroolsBeanFactory();
-        return dbf.getDrlFromExcel(filePath);
-
-    }
 }
