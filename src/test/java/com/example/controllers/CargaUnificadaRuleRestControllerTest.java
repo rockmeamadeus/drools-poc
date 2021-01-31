910b640a0aaf6a716c8b40bcf30eaf80fd5dc773
ar00062451 <ariel.vaca@prosegur.com>
2021-01-31 20:37:30 -0300

    .

--- src/test/java/com/example/controllers/CargaUnificadaRuleRestControllerTest.java
+++ src/test/java/com/example/controllers/CargaUnificadaRuleRestControllerTest.java
@@ -69,7 +69,7 @@ class CargaUnificadaRuleRestControllerTest {
 		ot.setCodTipoOT("ENT");
 		ot.setIdOT("1234");
 		ot.setCodOT("12345");
-		ot.setProducto("Entrega Divisa Extranjera");
+		ot.setProducto("Entrega de Otros");
 		ot.setEntidad("Santander");
 
 		Actividad actividad1 = new Actividad();
@@ -107,8 +107,10 @@ class CargaUnificadaRuleRestControllerTest {
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades", hasSize(4)))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].idOT", is(equalTo("1234"))))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].codOT", is(equalTo("12345"))))
-				.andExpect(jsonPath("$.servicioRutas[0].ots[0].producto", is(equalTo("Entrega Divisa Extranjera"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].producto", is(equalTo("Entrega de Otros"))))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].entidad", is(equalTo("Santander"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].valorMinimo", is(equalTo(101.12))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].valorMaximo", is(equalTo(751.123))))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[0].codActividad",
 						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(is(equalTo("SAFEBAG"))).or(equalTo("LecturaRemito"))))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[1].codActividad",
@@ -117,5 +119,84 @@ class CargaUnificadaRuleRestControllerTest {
 						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(is(equalTo("SAFEBAG"))).or(equalTo("LecturaRemito"))))
 				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[3].codActividad",
 						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(is(equalTo("SAFEBAG"))).or(equalTo("LecturaRemito"))));
+	}
+
+	@Test
+	void given_servicio_ruta_then_rule2_should_be_skipped() throws Exception {
+
+		MockMultipartFile file
+				= new MockMultipartFile(
+				"file",
+				"Ot-addActividad_multiple_rules_skip_rule2.xlsx",
+				MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+				Files.readAllBytes(Paths.get("src/test/resources/Ot-addActividad_multiple_rules_skip_rule2.xlsx"))
+		);
+
+
+		mockMvc.perform(multipart("/upload/").file(file))
+				.andExpect(status().isOk());
+
+		CargaUnificadaRuleRequest cargaUnificadaRuleRequest = new CargaUnificadaRuleRequest();
+
+		ServicioRuta servicioRuta = new ServicioRuta();
+
+		servicioRuta.setCodProducto("1");
+		servicioRuta.setCodServicio("29023URY");
+		servicioRuta.setIdServicio("73cd92b8-4cda-4084-8d20-bbf2cb064e03");
+
+		Ot ot = new Ot();
+		ot.setCodTipoOT("ENT");
+		ot.setIdOT("a50394de-2e7c-4db6-9b49-0f20397dc156");
+		ot.setCodOT("12345");
+		ot.setProducto("Entrega de Otros");
+		ot.setEntidad("Santander");
+
+		Actividad actividad1 = new Actividad();
+		actividad1.setCodActividad("SolicitudMotivoSinRemesa");
+		Actividad actividad2 = new Actividad();
+		actividad2.setCodActividad("Coleta");
+		Actividad actividad3 = new Actividad();
+		actividad3.setCodActividad("LecturaPrecintos");
+		Actividad actividad4 = new Actividad();
+		actividad4.setCodActividad("LecturaRemito");
+
+		ot.getActividades().add(actividad1);
+		ot.getActividades().add(actividad2);
+		ot.getActividades().add(actividad3);
+		ot.getActividades().add(actividad4);
+
+		servicioRuta.getOts().add(ot);
+		cargaUnificadaRuleRequest.getServicioRutas().add(servicioRuta);
+
+		mockMvc.perform(post("/cargaunificada/ruta/")
+				.content(new ObjectMapper().writeValueAsString(cargaUnificadaRuleRequest))
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.servicioRutas", is(notNullValue())))
+				.andExpect(jsonPath("$.servicioRutas").isArray())
+				.andExpect(jsonPath("$.servicioRutas", hasSize(1)))
+				.andExpect(jsonPath("$.servicioRutas[0].idServicio", is(equalTo("73cd92b8-4cda-4084-8d20-bbf2cb064e03"))))
+				.andExpect(jsonPath("$.servicioRutas[0].codServicio", is(equalTo("29023URY"))))
+				.andExpect(jsonPath("$.servicioRutas[0].codProducto", is(equalTo("1"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots", is(notNullValue())))
+				.andExpect(jsonPath("$.servicioRutas[0].ots").isArray())
+				.andExpect(jsonPath("$.servicioRutas[0].ots", hasSize(1)))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades", is(notNullValue())))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades").isArray())
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades", hasSize(4)))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].idOT", is(equalTo("a50394de-2e7c-4db6-9b49-0f20397dc156"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].codOT", is(equalTo("12345"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].producto", is(equalTo("Entrega de Otros"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].entidad", is(equalTo("Santander"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].valorMinimo", is(equalTo(101.12))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].valorMaximo", is(equalTo(751.123))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[0].codActividad",
+						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(equalTo("LecturaRemito")).or(equalTo("SolicitudMotivoSinRemesa"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[1].codActividad",
+						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(equalTo("LecturaRemito")).or(equalTo("SolicitudMotivoSinRemesa"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[2].codActividad",
+						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(equalTo("LecturaRemito")).or(equalTo("SolicitudMotivoSinRemesa"))))
+				.andExpect(jsonPath("$.servicioRutas[0].ots[0].actividades[3].codActividad",
+						either(is(equalTo("Coleta"))).or(is(equalTo("LecturaPrecintos"))).or(equalTo("LecturaRemito")).or(equalTo("SolicitudMotivoSinRemesa"))));
 	}
 }
